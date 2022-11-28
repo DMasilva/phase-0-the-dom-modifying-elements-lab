9ff913316454d4226b0566dbf3e27c237ec4d4ab
David <david.owuor@student.moringaschool.com>
2022-11-28 20:45:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main');
+
+document.querySelector('main').remove();
+
+document.getElementById('victory');
+
+const newHeader = document.getElementById('victory');
+
+newHeader.textContent='DAVY-MASILVA is the champion';
